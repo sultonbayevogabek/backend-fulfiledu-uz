6bd2be8f2880ed49a172cdc3d23c173f825e2df5
sultonbayevogabek <sultonbayevogabek@gmail.com>
2024-07-14 23:43:52 +0500

    commit

---
+++ public/js/excel.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let user = JSON.parse(localStorage.getItem('user'));
+
+if (user && user?.name && user?.phone && user?.time) {
+    const formData = new FormData();
+    formData.append('Ismi', user?.name);
+    formData.append('Telefon raqami', user?.phone);
+    formData.append(`Ro'yxatdan o'tgan vaqti`, user?.time);
+    fetch('https://script.google.com/macros/s/AKfycbz2Ef0tXR8ql-g7w9TjiI3i_PQmB3DY0AuA_O3MAAbrGO4sQfxibx90pDiugdBCb6yMfQ/exec', {
+        method: 'POST',
+        body: formData
+    })
+        .then(res => res.json())
+        .then(() => {
+            localStorage.clear();
+        })
+} else {
+    localStorage.clear();
+}
