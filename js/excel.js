424f96050ed3fa3bcf65e6e5bb2b2fae901cfd39
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-07-21 17:43:44 +0500

    commit

--- js/excel.js
+++ js/excel.js
@@ -7,7 +7,7 @@ if (user && user?.name && user?.phone && user?.time) {
     formData.append('Ismi', user?.name);
     formData.append('Telefon raqami', user?.phone);
     formData.append(`Ro'yxatdan o'tgan vaqti`, user?.time);
-    fetch('https://script.google.com/macros/s/AKfycbw1lUCW-ZvGpZTDl-QBsp1mKegF_RjlqS1EZEpr8KzSqrOGG9MSjdG5z0PMorVP1-Z8Dw/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbyV2b_CW3DwoJK9u-IYRjewq1K-WPyi8uW2nHTfVX2_lIoVi3xFilbJa79qDHV5A13p/exec', {
         method: 'POST',
         body: formData
     })
--- js/script.js
+++ js/script.js
@@ -2,97 +2,68 @@
 
 // Index page
 try {
-    const registerButtons = document.querySelectorAll('.webinar-main-button');
-    const modalBackdrop = document.querySelector('.modal-backdrop');
-    const modalCloserElements = document.querySelectorAll('[data-modal-close]');
-    const form = document.querySelector('.form');
-    const formAlert = document.querySelector('.form-alert');
+  const registerButtons = document.querySelectorAll('.webinar-main-button');
+  const modalBackdrop = document.querySelector('.modal-backdrop');
+  const modalCloserElements = document.querySelectorAll('[data-modal-close]');
+  const form = document.querySelector('.form');
+  const formAlert = document.querySelector('.form-alert');
 
-    registerButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            modalBackdrop.classList.add('modal-backdrop--open')
-        })
+  registerButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      modalBackdrop.classList.add('modal-backdrop--open')
     })
+  })
 
-    function closeModal() {
-        modalBackdrop.classList.remove('modal-backdrop--open');
+  function closeModal() {
+    modalBackdrop.classList.remove('modal-backdrop--open');
+  }
+
+  window.addEventListener('keydown', e => {
+    if (e.key === 'Escape') {
+      closeModal();
+    }
+  })
+
+  modalCloserElements.forEach(el => {
+    el.addEventListener('click', e => {
+      if (e.target.hasAttribute('data-modal-close')) {
+        closeModal();
+      }
+    })
+  })
+
+  form.addEventListener('submit', async e => {
+    e.preventDefault();
+
+    const submitButton = e.target.querySelector('.form-button');
+    const name = e.target.querySelector('#name').value.trim();
+    const phone = e.target.querySelector('#phone').value?.replace(/[^0-9]/g, '');
+
+    if (!name.length) {
+      formAlert.textContent = 'Ismingizni kiriting';
+      formAlert.classList.add('open');
+      return;
     }
 
-    window.addEventListener('keydown', e => {
-        if (e.key === 'Escape') {
-            closeModal();
-        }
-    })
+    if (phone?.length !== 12) {
+      formAlert.textContent = 'Telefon raqamingizni kiriting';
+      formAlert.classList.add('open');
+      return;
+    }
 
-    modalCloserElements.forEach(el => {
-        el.addEventListener('click', e => {
-            if (e.target.hasAttribute('data-modal-close')) {
-                closeModal();
-            }
-        })
-    })
+    if (name.length && phone?.length === 12) {
+      submitButton.setAttribute('disabled', true);
+      submitButton.textContent = 'Yuborilmoqda...'
 
-    form.addEventListener('submit', async e => {
-        e.preventDefault();
+      localStorage.setItem('user', JSON.stringify({
+        name,
+        phone,
+        time: new Date().toLocaleString()
+      }))
 
-        const submitButton = e.target.querySelector('.form-button');
-        const name = e.target.querySelector('#name').value.trim();
-        const phone = e.target.querySelector('#phone').value?.replace(/[^0-9]/g, '');
-
-        if (!name.length) {
-            formAlert.textContent = 'Ismingizni kiriting';
-            formAlert.classList.add('open');
-            return;
-        }
-
-        if (phone?.length !== 12) {
-            formAlert.textContent = 'Telefon raqamingizni kiriting';
-            formAlert.classList.add('open');
-            return;
-        }
-
-        if (name.length && phone?.length === 12) {
-            submitButton.setAttribute('disabled', true);
-            submitButton.textContent = 'Yuborilmoqda...'
-
-            localStorage.setItem('user', JSON.stringify({
-                name,
-                phone,
-                time: new Date().toLocaleString()
-            }))
-
-            formAlert.classList.remove('open');
-            window.location.href = `../telegram.html`
-        }
-    })
+      formAlert.classList.remove('open');
+      window.location.href = `../telegram.html`
+    }
+  })
 } catch (e) {
 }
-
-
-const url = 'https://voicerss-text-to-speech.p.rapidapi.com/?key=%3CREQUIRED%3E';
-const options = {
-    method: 'POST',
-    headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        'X-RapidAPI-Key': '4008893a79msh51b668c65f6f357p170956jsn28e222d14bd9',
-        'X-RapidAPI-Host': 'voicerss-text-to-speech.p.rapidapi.com'
-    },
-    body: new URLSearchParams({
-        src: 'Hello, world!',
-        hl: 'en-us',
-        r: '0',
-        c: 'mp3',
-        f: '8khz_8bit_mono'
-    })
-};
-
-
-(async () => {
-    try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-    }
-})()
